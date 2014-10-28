ab54dc8e701e1981bc9682986a26c656f3c98fcf
Mari Wahl <mari.wahl9@gmail.com>
2014-10-27 20:10:17 -0400

    some math scripts

---
+++ Cryptography/tools/finding_gcd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+def finding_gcd(a, b):
+    ''' implements the greatest common divider algorithm '''
+    while(b != 0):
+        result = b
+        a, b = b, a % b        
+    return result
+
+
+def test_finding_gcd():
+    number1 = 21
+    number2 = 12
+    assert(finding_gcd(number1, number2) == 3)
+    print('Tests passed!')
+
+if __name__ == '__main__':
+    test_finding_gcd()
+
+
+
+    
+    
+    
+    
